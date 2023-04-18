911f2b33b6593a88b9b93f39ed5c7ad4986d926c
Het <het.patel@yudiz.com>
2023-04-18 21:07:45 +0530

    demoReadDirectory added

--- demoReadDirectory.js
+++ demoReadDirectory.js
@@ -3,48 +3,54 @@ const url = require("url");
 const http = require("http");
 const prompt = require("prompt-sync")();
 let basedir = "C:/Users/91720/Desktop/B-Square";
+let fetchedFiles = [];
 
 //////////////////////////* Input using terminal *////////////////////////////////
+try {
+
+let path = prompt("Enter directory name: ");
+
+  getFilesByFolderName(path);
+
+} catch (error) {
+  console.log(error);
+}
+
+////////////////////////////* Input using POSTMAN *///////////////////////////////
 // try {
+//   http
+//     .createServer((req, res) => {
+//       let parsedUrl = url.parse(req.url);
+//       let path = parsedUrl.pathname;
 
-  // let path = prompt("Enter directory name: ");
-
-//   getFilesByFolderName(path);
-
+//       req.on("end", () => {
+//         if (req.method === "GET") {
+//           getFilesByFolderName(path);
+//         }
+//       });
+//       res.end(`result: ${fetchedFiles}`);
+//     })
+//     .listen(8080, () => {
+//       console.log("Listening on port 8080");
+//     });
 // } catch (error) {
 //   console.log(error);
 // }
 
-////////////////////////////* Input using POSTMAN *///////////////////////////////
-http.createServer((req, res) => {
-  let parsedUrl = url.parse(req.url);
-  let path = parsedUrl.pathname;
-
-    // let basepath = basedir + path;
-    // console.log(basepath);
-  
-  // req.on("end", () => {
-  //   if (req.method === "GET") {
-      getFilesByFolderName(path);
-  //   }
-  // });
-  res.end("files"); 
-}).listen(8080, () => {
-    console.log("Listening on port 8080");
-  });
-
 
 /////////////////////////////////* function */////////////////////////////////////
 function getFilesByFolderName(path) {
-  let basepath = basedir + path;
+  let basepath = basedir + "/" + path;
   console.log(basepath);
-  // fs.readdir(basepath, (error, files) => {
-  //   if (error) {
-  //     console.log(error);
-  //   } else {
-  //    files.forEach((files) => console.log(files));
-  //   //  return files;
-  //   }
-  // });
-  // return files;
+  fs.readdir(basepath, (error, files) => {
+    if (error) {
+      console.log(error);
+    } else {
+      files.forEach((file) => {
+        // console.log(file)
+        fetchedFiles.push(file);
+      });
+      console.log(fetchedFiles);
+    }
+  });
 }
